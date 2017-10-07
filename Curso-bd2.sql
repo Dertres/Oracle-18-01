c72001c0214074c46413cdf99c61d47cbece3339
Oscar Neri Fabián <lm777fmb3@hotmail.com>
2017-10-07 09:41:28 -0500

    Add files via upload

---
+++ Curso-bd2.sql
@@ -0,0 +1,10 @@
+--07/10/17
+
+create table Almacen(
+Numero_almacen integer,
+Ubicacion_almacen varchar2(80),
+constraint PK_Numero_almacen primary key(Numero_almacen)
+--constraint | nombre de constraint | tipo de constraint(nombre de cmapo)
+);
+
+--Las constraint son restricciones
